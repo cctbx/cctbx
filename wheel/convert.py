dd70f51bca6da5a0f72a113b91ae865dd4a9b4ad
Billy K. Poon <bkpoon@lbl.gov>
2025-05-06 22:46:10 -0700

    Make bin files executable

--- wheel/convert.py
+++ wheel/convert.py
@@ -106,6 +106,8 @@ class CondaWheelConverter():
     if dest is not None:
       os.makedirs(dest.parent, exist_ok=True)
       shutil.copyfile(file_path, dest)
+      if dest in self.bin_files:
+        os.chmod(dest, 0o755)
       print(f'''\
     Copying {file_path}
             {dest}\
